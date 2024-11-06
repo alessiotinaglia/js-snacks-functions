d0a5f599544a1c4ef78886dace4ac47ef71c9c0e
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-06 15:11:42 +0100

    fine primo es

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -5,9 +5,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function oneLetter(myArrey){        
+    const newArrey = [];
+    for (let i = 0; i < myArrey.length; i++){
+        newArrey.push(myArrey[i].charAt(0));
+    }
+
+    return newArrey;    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const arrayFinale = oneLetter(names);
+console.log(arrayFinale);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
